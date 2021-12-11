df0f03b30ff8ac30ec05bbfa35d0c7f0b7797da6
K.Kiyokawa <koichi20110068@gmail.com>
2021-12-12 00:27:47 +0900

    chore: remove unnecessary tailwind config

--- remix-sandbox/tailwind.config.js
+++ remix-sandbox/tailwind.config.js
@@ -4,7 +4,6 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./app/**/*.{ts,tsx}"],
-  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
