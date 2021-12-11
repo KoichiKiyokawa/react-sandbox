cc28c8b23bf03c2b6cd153896f1490d6735caca2
K.Kiyokawa <koichi20110068@gmail.com>
2021-12-11 15:00:19 +0900

    feat: article index

--- remix-sandbox/app/domains/article/ArticleCard.tsx
+++ remix-sandbox/app/domains/article/ArticleCard.tsx
@@ -1,13 +1,27 @@
-import { Article } from "@prisma/client";
+import { Article, Tag, User } from "@prisma/client";
+import dayjs from "dayjs";
+import { Link } from "remix";
 
-type Props = {
-  article: Article;
-};
+export type ArticleWithAuthorAndTag = Article & { author: Pick<User, "id" | "name">; tags: Tag[] };
 
-export const ArticleCard = ({ article }: Props) => {
+type Props = { data: ArticleWithAuthorAndTag };
+
+const MAX_BODY_SHOW_LENGTH = 15;
+
+export const ArticleCard = ({ data }: Props) => {
   return (
     <div>
-      <h2>{article.title}</h2>
+      <div>
+        <p className="text-primary">{data.author.name}</p>
+        <p className="text-[#BBBBBB] text-sm">{dayjs(data.createdAt).format("YYYY-MM-DD")}</p>
+      </div>
+
+      <Link to={`/articles/${data.slug}`}>
+        <h2 className="font-bold text-[1.5rem]">{data.title}</h2>
+        <p className="text-[#999999]">{data.body.slice(0, MAX_BODY_SHOW_LENGTH)}</p>
+
+        <span className="text-[#BBBBBB]">Read more...</span>
+      </Link>
     </div>
   );
 };
--- remix-sandbox/app/routes/index.tsx
+++ remix-sandbox/app/routes/index.tsx
@@ -1,14 +1,20 @@
-import { Link } from "remix";
-import { Article } from "@prisma/client";
+import { Link, useLoaderData } from "remix";
 import { db } from "~/utils/db.server";
+import { ArticleCard, ArticleWithAuthorAndTag } from "~/domains/article/ArticleCard";
 
-type LoaderData = { articles: Article[] };
+type LoaderData = { articles: ArticleWithAuthorAndTag[] };
 
 export const loader = async (): Promise<LoaderData> => {
-  return { articles: await db.article.findMany({ include: { tags: true } }) };
+  return {
+    articles: await db.article.findMany({
+      include: { author: { select: { id: true, name: true } }, tags: true },
+    }),
+  };
 };
 
 export default function Index() {
+  const { articles } = useLoaderData<LoaderData>();
+
   return (
     <>
       {/* Hero */}
@@ -17,15 +23,23 @@ export default function Index() {
         <p>A place to share your knowledge.</p>
       </div>
 
-      <ul>
-        <li>
-          <Link to="/">Global Feed</Link>
-        </li>
-      </ul>
+      {/* Tab */}
+      <div className="container mx-auto">
+        <ul className="flex border-b border-gray-100">
+          <li className="text-primary border-b border-primary">
+            <Link to="/" className="block py-2">
+              Global Feed
+            </Link>
+          </li>
+        </ul>
 
-      <Link to="/users" className="text-link">
-        users
-      </Link>
+        {/* Articles */}
+        {articles.map((article) => (
+          <div key={article.slug} className="py-6 border-b border-gray-300 last:border-b-0">
+            <ArticleCard data={article} />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
