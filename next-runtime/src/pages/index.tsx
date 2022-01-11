18373b7bcafe5b37e328668589fd8bc659945858
K.Kiyokawa <koichi20110068@gmail.com>
2022-01-11 12:28:30 +0900

    refactor(next-runtime): use document.forms.namedItem

--- next-runtime/src/pages/index.tsx
+++ next-runtime/src/pages/index.tsx
@@ -13,7 +13,6 @@ import { Post } from "@prisma/client"
 import { NextPage } from "next"
 import { handle, json, redirect } from "next-runtime"
 import { Form, useFormSubmit } from "next-runtime/form"
-import { useRef } from "react"
 import { db } from "../utils/db.server"
 
 type Props = {
@@ -32,16 +31,14 @@ export const getServerSideProps = handle<Props, {}, Post>({
 })
 
 const Home: NextPage<Props> = ({ posts }) => {
-  const formRef = useRef<HTMLFormElement>()
   const { isSubmitting } = useFormSubmit()
   const onSuccess = () => {
-    formRef.current?.reset()
+    document.forms.namedItem("postForm")?.reset()
   }
 
   return (
     <Container>
-      {/* @ts-expect-error */}
-      <Form method="post" ref={formRef} onSuccess={onSuccess}>
+      <Form method="post" name="postForm" onSuccess={onSuccess}>
         <VStack align="flex-start" spacing={4}>
           <FormControl>
             <FormLabel htmlFor="title">title</FormLabel>
