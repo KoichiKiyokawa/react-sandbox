e6afb5ac3943a233ca21fe6e6d9d739196f6a148
K.Kiyokawa <koichi20110068@gmail.com>
2022-01-11 15:47:19 +0900

    chore(next-runtime): seed

--- next-runtime/prisma/seed.ts
+++ next-runtime/prisma/seed.ts
@@ -1,12 +1,24 @@
 import { db } from "src/utils/db.server"
+import { toRange } from "rhodash"
 
 async function reset() {
+  await db.comment.deleteMany()
   await db.post.deleteMany()
 }
 
 async function seed() {
   for (let i = 1; i <= 10; i++) {
-    await db.post.create({ data: { title: `title${i}`, body: `body${i}` } })
+    await db.post.create({
+      data: {
+        title: `title${i}`,
+        body: `body${i}`,
+        comments: {
+          create: toRange(1, 10).map((j) => ({
+            text: `post-${i}-comment-${j}`,
+          })),
+        },
+      },
+    })
   }
 }
 
