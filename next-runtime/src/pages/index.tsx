82e84e39675c71657ff3a760a7eb6bba776e09ec
K.Kiyokawa <koichi20110068@gmail.com>
2022-01-11 12:22:32 +0900

    feat(next-runtime): index page

--- next-runtime/src/pages/index.tsx
+++ next-runtime/src/pages/index.tsx
@@ -1,24 +1,63 @@
-import { NextPage } from "next"
-import { Button, Container, ListItem, UnorderedList } from "@chakra-ui/react"
-import { handle, json } from "next-runtime"
-import { db } from "../utils/db.server"
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  ListItem,
+  Textarea,
+  UnorderedList,
+  VStack,
+} from "@chakra-ui/react"
 import { Post } from "@prisma/client"
-
-export const getServerSideProps = handle({
-  async get() {
-    const posts = await db.post.findMany()
-    return json({ posts })
-  },
-})
+import { NextPage } from "next"
+import { handle, json, redirect } from "next-runtime"
+import { Form, useFormSubmit } from "next-runtime/form"
+import { useRef } from "react"
+import { db } from "../utils/db.server"
 
 type Props = {
   posts: Post[]
 }
 
+export const getServerSideProps = handle<Props, {}, Post>({
+  async get() {
+    const posts = await db.post.findMany()
+    return json({ posts })
+  },
+  async post({ req: { body } }) {
+    await db.post.create({ data: body })
+    return redirect("/")
+  },
+})
+
 const Home: NextPage<Props> = ({ posts }) => {
+  const formRef = useRef<HTMLFormElement>()
+  const { isSubmitting } = useFormSubmit()
+  const onSuccess = () => {
+    formRef.current?.reset()
+  }
+
   return (
     <Container>
-      <Button>OK</Button>
+      {/* @ts-expect-error */}
+      <Form method="post" ref={formRef} onSuccess={onSuccess}>
+        <VStack align="flex-start" spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor="title">title</FormLabel>
+            <Input id="title" name="title"></Input>
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="body">body</FormLabel>
+            <Textarea id="body" name="body"></Textarea>
+          </FormControl>
+
+          <Button type="submit" disabled={isSubmitting}>
+            OK
+          </Button>
+        </VStack>
+      </Form>
 
       <UnorderedList>
         {posts.map((post) => (
