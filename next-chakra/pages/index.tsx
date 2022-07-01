6a3b7eabc15e367a92af1d35faf4119a6b3c76f9
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-07-01 19:45:46 +0900

    feat: add `getServerSideProps`

--- next-chakra/pages/index.tsx
+++ next-chakra/pages/index.tsx
@@ -1,9 +1,21 @@
-import { Button, Container } from "@chakra-ui/react";
+import { Button, Container, Text } from "@chakra-ui/react"
+import { GetServerSideProps } from "next"
 
-export default function IndexPage() {
+type Props = { message: string }
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  return {
+    props: {
+      message: "hoge",
+    },
+  }
+}
+
+export default function IndexPage({ message }: Props) {
   return (
     <Container>
+      <Text>{message}</Text>
       <Button>OK</Button>
     </Container>
-  );
+  )
 }
