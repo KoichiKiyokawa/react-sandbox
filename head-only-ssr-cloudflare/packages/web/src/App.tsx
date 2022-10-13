0fdfe029d4eec5768ac0c89fd1101b1bca3d8009
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-10-13 22:53:47 +0900

    chore: brushup eslint config

--- head-only-ssr-cloudflare/packages/web/src/App.tsx
+++ head-only-ssr-cloudflare/packages/web/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import reactLogo from "./assets/react.svg";
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -27,6 +27,6 @@ function App() {
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
-}
+};
 
 export default App;
