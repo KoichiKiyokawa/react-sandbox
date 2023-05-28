f3aadb6fabb1b4e43ff409e10a2d34f125937091
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-05-28 11:38:29 +0900

    fix: d1 binding

--- cloudflare-next-on-pages-d1/lib/db/index.ts
+++ cloudflare-next-on-pages-d1/lib/db/index.ts
@@ -1,11 +1,3 @@
 import { drizzle } from "drizzle-orm/d1"
 
-interface Env {
-  DB: D1Database
-}
-
-declare global {
-  const env: Env
-}
-
-export const db = drizzle(env.DB)
+export const db = drizzle(process.env.DB)
