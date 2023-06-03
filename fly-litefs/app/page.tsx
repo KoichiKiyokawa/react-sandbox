563987bf8774cff0678c4db3ca75e24d976362c7
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-06-03 15:34:53 +0900

    feat: setup litefs

--- fly-litefs/app/page.tsx
+++ fly-litefs/app/page.tsx
@@ -1,9 +1,12 @@
 import { db } from "@/lib/db"
 import Image from "next/image"
 import bigImage from "@/public/big.jpg"
+import { cookies } from "next/headers"
 
 export default async function Home() {
+  cookies() // to disable static render
   const posts = await db.post.findMany()
+
   return (
     <div>
       <Image src={bigImage} alt="wallpaper" placeholder="blur" />
