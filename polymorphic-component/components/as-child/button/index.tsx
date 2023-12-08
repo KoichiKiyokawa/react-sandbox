4a1d39b5692a448fc785da32f87c4927034e0207
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-12-09 00:23:16 +0900

    fix: disable link by render div

--- polymorphic-component/components/as-child/button/index.tsx
+++ polymorphic-component/components/as-child/button/index.tsx
@@ -1,22 +1,27 @@
-import { cloneElement, forwardRef, isValidElement } from "react";
-import styles from "../../render/button/index.module.css";
-import clsx from "clsx";
+import { cloneElement, forwardRef, isValidElement } from "react"
+import styles from "../../render/button/index.module.css"
+import clsx from "clsx"
 
 export type ButtonProps = {
-  variant?: "primary" | "secondary";
-  disabled?: boolean;
-  leftIcon?: React.ReactElement;
-  rightIcon?: React.ReactElement;
-} & ({ asChild: true; children: React.ReactElement } | ({ asChild?: false } & React.ComponentPropsWithRef<"button">));
+  variant?: "primary" | "secondary"
+  disabled?: boolean
+  leftIcon?: React.ReactElement
+  rightIcon?: React.ReactElement
+} & ({ asChild: true; children: React.ReactElement } | ({ asChild?: false } & React.ComponentPropsWithRef<"button">))
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
   { variant = "priamry", disabled, leftIcon, rightIcon, asChild = false, children, ...buttonProps },
   ref
 ) {
-  const shouldActAsChild = asChild && isValidElement(children);
+  const shouldActAsChild = asChild && isValidElement(children)
 
   return cloneElement(
-    shouldActAsChild ? children : <button ref={ref} type="button" disabled={disabled} {...buttonProps} />,
+    shouldActAsChild ? (
+      // prettier-ignore
+      disabled ? <div /> : children
+    ) : (
+      <button ref={ref} type="button" disabled={disabled} {...buttonProps} />
+    ),
     {
       "data-variant": variant,
       className: clsx(
@@ -24,12 +29,12 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
         shouldActAsChild && children.props.className,
         "className" in buttonProps && buttonProps.className
       ),
-      ...(disabled && { "aria-disabled": true, href: undefined }),
+      ...(disabled && { "aria-disabled": true }),
     },
     leftIcon ? <span className={styles.leftIcon}>{rightIcon}</span> : null,
     shouldActAsChild ? children.props.children : children,
     rightIcon ? <span className={styles.rightIcon}>{rightIcon}</span> : null
-  );
-}) as React.FC<ButtonProps>;
+  )
+}) as React.FC<ButtonProps>
 
-export default Button;
+export default Button
