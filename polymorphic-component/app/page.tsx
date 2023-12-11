e1308047725379abe14cc6b6f0240aaa9297157f
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-12-11 19:52:03 +0900

    fea: set invalid a tag in button on purpose

--- polymorphic-component/app/page.tsx
+++ polymorphic-component/app/page.tsx
@@ -1,23 +1,27 @@
-"use client";
+"use client"
 
-import RenderButton from "@/components/render/button";
-import AsChildButton from "@/components/as-child/button";
-import { useEffect, useRef } from "react";
+import RenderButton from "@/components/render/button"
+import AsChildButton from "@/components/as-child/button"
+import { useEffect, useRef } from "react"
 
 export default function Home() {
-  const buttonRef = useRef<HTMLButtonElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null)
 
   useEffect(() => {
-    buttonRef.current?.focus();
-  }, []);
+    buttonRef.current?.focus()
+  }, [])
 
   return (
     <div>
+      {/* invalid: a should not be in a button tag */}
+      <button>
+        <a href="https://example.com">a</a>
+      </button>
       <RenderButton ref={buttonRef}>hoge</RenderButton>
       <AsChildButton disabled>as child</AsChildButton>
       <AsChildButton asChild disabled>
         <a href="https://example.com">as child</a>
       </AsChildButton>
     </div>
-  );
+  )
 }
