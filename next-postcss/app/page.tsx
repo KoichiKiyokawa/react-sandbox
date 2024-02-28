1da388aa95e8b1ad2341f74b63c590426aca9055
Koichi Kiyokawa <koichi20110068@gmail.com>
2024-02-29 01:23:58 +0900

    :tada: init next-postcss

---
+++ next-postcss/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>
+        hogehogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge
+      </h1>
+    </main>
+  );
+}
---
+++ next-postcss/postcss.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  plugins: {
+    "postcss-flexbugs-fixes": {},
+    "postcss-import": {
+      path: [path.resolve(__dirname, "styles")],
+    },
+    "postcss-mixins": {},
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 2,
+    },
+  },
+};
